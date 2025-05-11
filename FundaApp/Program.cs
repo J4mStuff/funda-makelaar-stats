36d01404bc2c1b525675bf24169846f5d6a3a7e1
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2025-05-11 12:20:10 +0200

    first sample request working

---
+++ FundaApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FundaApp;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var sharedClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://partnerapi.funda.nl/feeds/Aanbod.svc/"),
+        };
+        GetAsync(sharedClient).Wait();
+    }
+
+    private static async Task GetAsync(HttpClient httpClient)
+    {
+        var key = await File.ReadAllTextAsync("data/secret.txt");
+        const string purchase = "koop";
+        const string garden = "tuin";
+        //const string agent = "makelaar";
+        
+        var uri = httpClient.BaseAddress + $"json/{key}/?type={purchase}&zo=/amsterdam/{garden}/&page=1&pagesize=25";
+        //var uri = httpClient.BaseAddress + $"json/{key}/?type={purchase}&zo=/amsterdam/&page=1&pagesize=25"; 
+        
+        using var response = await httpClient.GetAsync(uri);
+        
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"{jsonResponse}\n");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # funda-makelaar-stats
-Assignment
+Coding assignment
+
+
+To Run:
+create a file <FundaApp/data/secret.txt> before compiling.
+It should hold the api key string. The web requests will fail without it.
